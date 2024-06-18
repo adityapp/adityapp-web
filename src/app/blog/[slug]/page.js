07af51e03f0394eb8950de10c8376fef375530ad
adityapp <aditpp97@gmail.com>
2024-06-18 21:11:02 +0700

    try build react markdown

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,18 +1,20 @@
 import fs from "fs"
-import ReactMarkdown from 'react-markdown'
+import Markdown from "react-markdown"
 import remarkGfm from 'remark-gfm'
+import matter from "gray-matter"
 
 export default function BlogContentPage(props){
   const getBlogContent = (slug) => {
     const folder = "blogs/"
     const file = `${folder}${slug}.md`
     const content = fs.readFileSync(file, "utf8")
-    return content
+    const matterResult = matter(content)
+    return matterResult.content
   }
 
   const content = getBlogContent(props.params.slug)
   
   return(
-    <ReactMarkdown className="text-white" remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+    <article><Markdown className="text-white" remarkPlugins={[remarkGfm]}>{content}</Markdown></article>
   )
 }
